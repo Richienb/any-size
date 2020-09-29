d97ecb58c617c5e73bdf1927073ecbfbba916f5f
Richie Bendall <richiebendall@gmail.com>
2020-09-30 03:39:44 +1300

    Test empty string
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ test("main", t => {
 	t.is(size({ a: 1, b: 2, c: 3, [Symbol("d")]: 4 }), 4)
 	t.is(size([1, 2, 3, 4, 5]), 5)
 	t.is(size("unicorns"), 8)
+	t.is(size(""), 0)
 	t.is(size(12345.678), 9)
 	t.is(size(new Map([["a", 1], ["b", 2], ["c", 3]])), 3)
 	t.is(size(new Set([1, 2, 3, 4, 5])), 5)
